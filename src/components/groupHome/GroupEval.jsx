4e8d9f620925398373770c650bde1ff00b513f9b
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-08 22:39:10 +0900

    groupEval UI 틀  구현 완료

--- src/components/groupHome/GroupEval.jsx
+++ src/components/groupHome/GroupEval.jsx
@@ -1,3 +1,6 @@
+import "./GroupEval.css";
+import Button from "../Button";
+
 function calTime(time) {
     time = new Date(time); // getTime은 ms이기에 get~매서드 쓰려면 다시 new Date로 date로 변환해줘야 함
     const year = time.getFullYear() % 100;
@@ -5,7 +8,7 @@ function calTime(time) {
     const month =
         time.getMonth() < 9 ? `0${time.getMonth() + 1}` : time.getMonth() + 1;
     // 한자리 숫자는 앞에 0 붙이게 함
-    const date = time.getDate() < 9 ? `0${time.getDate() + 1}` : time.getDate();
+    const date = time.getDate() < 9 ? `0${time.getDate()}` : time.getDate();
 
     return year + "/" + month + "/" + date;
 }
@@ -14,12 +17,19 @@ const GroupEval = () => {
     // new Date : 각각 Year, Month, Date 받아오면 2022/01/01 -> 2021/12/31 각 기준점 넘는거 구현해줘야 함
     // getTime -> new Date : 경계선 넘어가는거 알아서 계산됨 (get~ 매서드로 Year, Month, Date 받아오기만 하면 됨)
     const today = new Date().getTime();
+
+    // 짜피 일욜만 열릴거니까, '현재 시각 - 7일'으로만 논리짬 (일욜(고정)-7일 이니까~)
     const startDate = calTime(today - 7 * 1000 * 60 * 60 * 24);
     const endDate = calTime(today);
     return (
-        <div>
+        <div className="GroupEval">
             <h3>5월 셋째주 그룹 평가</h3>
-            <div>{`${startDate} ~ ${endDate}`}</div>
+            <div className="date">{`${startDate} - ${endDate}`}</div>
+            <section className="eval_section">
+                <div className="me">본인</div>
+                <div className="other">다른 사람</div>
+            </section>
+            <Button text={"저장"} type={"save"} />
         </div>
     );
 };

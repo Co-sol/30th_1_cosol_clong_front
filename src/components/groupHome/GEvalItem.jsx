b6bdc791b393227976bbb98caae7e111c4ac1b10
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-09 14:13:41 +0900

    그룹 평가 item 별 선택 시 색칠 구현

--- src/components/groupHome/GEvalItem.jsx
+++ src/components/groupHome/GEvalItem.jsx
@@ -1,8 +1,16 @@
 import "./GEvalItem.css";
 import { getBadgeImage } from "../../utils/get-badge-images";
+import EmptyStar_img from "../../assets/EmptyStar_img.PNG";
 import FullStar_img from "../../assets/FullStar_img.PNG";
+import StarRating from "./StarRating";
+import { useState } from "react";
 
 const GEvalItem = ({ person }) => {
+    const [isClick, setIsClick] = useState(0);
+    const onClickStar = (n) => {
+        setIsClick(n);
+    };
+
     return (
         <div className="GEvalItem">
             <img src={getBadgeImage(person.badgeId)} />
@@ -20,20 +28,40 @@ const GEvalItem = ({ person }) => {
                 </div>
             </section>
             <div className="rating">
-                <button className="star">
-                    <img src={FullStar_img} />
+                <button className="star" onClick={() => onClickStar(1)}>
+                    {isClick >= 1 ? (
+                        <img src={FullStar_img} />
+                    ) : (
+                        <img src={EmptyStar_img} />
+                    )}
                 </button>
-                <button className="star">
-                    <img src={FullStar_img} />
+                <button className="star" onClick={() => onClickStar(2)}>
+                    {isClick >= 2 ? (
+                        <img src={FullStar_img} />
+                    ) : (
+                        <img src={EmptyStar_img} />
+                    )}
                 </button>
-                <button className="star">
-                    <img src={FullStar_img} />
+                <button className="star" onClick={() => onClickStar(3)}>
+                    {isClick >= 3 ? (
+                        <img src={FullStar_img} />
+                    ) : (
+                        <img src={EmptyStar_img} />
+                    )}
                 </button>
-                <button className="star">
-                    <img src={FullStar_img} />
+                <button className="star" onClick={() => onClickStar(4)}>
+                    {isClick >= 4 ? (
+                        <img src={FullStar_img} />
+                    ) : (
+                        <img src={EmptyStar_img} />
+                    )}
                 </button>
-                <button className="star">
-                    <img src={FullStar_img} />
+                <button className="star" onClick={() => onClickStar(5)}>
+                    {isClick >= 5 ? (
+                        <img src={FullStar_img} />
+                    ) : (
+                        <img src={EmptyStar_img} />
+                    )}
                 </button>
             </div>
         </div>

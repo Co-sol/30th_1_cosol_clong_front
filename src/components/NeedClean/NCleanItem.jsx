61fac373ce62c4a49ed0b4aec335c1b42aca1422
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-12 00:12:15 +0900

    가장 청소가 필요한 공간 구현 완료

--- src/components/NeedClean/NCleanItem.jsx
+++ src/components/NeedClean/NCleanItem.jsx
@@ -3,9 +3,12 @@ import "./NCleanItem.css";
 const NCleanItem = ({ item, idx }) => {
     return (
         <div className="NCleanItem">
-            <div>{idx + 1}</div>
-            <div>{item.place}</div>
-            <div>{item.cnt}</div>
+            <div className="rank">{idx + 1}</div>
+            <div className="place">{item.place}</div>
+            <div className="cnt">
+                {item.cnt}
+                <div className="unit">{"개"}</div>
+            </div>
         </div>
     );
 };

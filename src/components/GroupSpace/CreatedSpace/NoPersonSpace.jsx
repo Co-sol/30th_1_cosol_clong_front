b6bfae1ae5ae5c8daf56add441f21fa01345336d
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-17 13:31:42 +0900

    반응형 바꾸다가 오류남

--- src/components/GroupSpace/CreatedSpace/NoPersonSpace.jsx
+++ src/components/GroupSpace/CreatedSpace/NoPersonSpace.jsx
@@ -13,17 +13,17 @@ const NoPersonSpace = () => {
         >
             <h3
                 style={{
-                    fontSize: "27px",
+                    fontSize: "clamp(24px, 1.88vw, 27px)",
                     fontFamily: "bold",
-                    marginBottom: "24px",
+                    marginBottom: "clamp(21px, 1.67vw, 24px)",
                 }}
             >
                 내 공간이 존재하지 않아요
             </h3>
             <div
                 style={{
-                    fontSize: "22px",
-                    marginBottom: "50px",
+                    fontSize: "clamp(20px, 1.53vw, 22px)",
+                    marginBottom: "clamp(45px, 3.47vw, 50px)",
                 }}
             >
                 공간을 생성해볼까요?
@@ -31,14 +31,14 @@ const NoPersonSpace = () => {
             <button
                 style={{
                     color: "white",
-                    fontSize: "27px",
+                    fontSize: "clamp(24px, 1.88vw, 27px)",
                     fontFamily: "bold",
 
                     border: "none",
-                    borderRadius: "15px",
+                    borderRadius: "clamp(12px, 1.04vw, 15px)",
                     backgroundColor: "#8BE2B6",
-                    width: "300px",
-                    height: "60px",
+                    width: "clamp(267px, 20.83vw, 300px)",
+                    height: "clamp(54px, 4.17vw, 60px)",
                 }}
             >
                 생성하기

fb9385eddca0c3a5b633e8c3de875e4088e0350f
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-17 19:31:02 +0900

    채연언니 그룹공간 피드백 중1

--- src/components/GroupSpace/NeedClean/NCleanItem.jsx
+++ src/components/GroupSpace/NeedClean/NCleanItem.jsx
@@ -3,8 +3,10 @@ import "./NCleanItem.css";
 const NCleanItem = ({ item, idx }) => {
     return (
         <div className="NCleanItem">
-            <div className="rank">{idx + 1}</div>
-            <div className="place">{item.place}</div>
+            <div className="rankName">
+                <div className="rank">{idx + 1}</div>
+                <div className="place">{item.place}</div>
+            </div>
             <div className="cnt">
                 {item.cnt}
                 <div className="unit">{"개"}</div>

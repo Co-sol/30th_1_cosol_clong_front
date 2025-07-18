6aaadbcea1d63c3239005102ab6414df819d0f57
nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-19 01:52:41 +0900

    PListAddModal 반응형 완료, DropDown 할 차례

--- src/components/GroupSpace/CheckList/Person/DropDown.jsx
+++ src/components/GroupSpace/CheckList/Person/DropDown.jsx
@@ -20,9 +20,9 @@ const DropDown = ({ title, targetPlaceData, setCreateData, createData }) => {
     return (
         <Dropdown
             style={{
-                marginBottom: "min(2.1vw, 30.24px)",
-                width: "min(9.03vw, 130px)", // 130 ÷ 14.4 = 9.03vw
-                height: "min(2.99vw, 43px)", // 43 ÷ 14.4
+                marginBottom: "clamp(23.82px ,1.76vw ,30.24px)",
+                width: "clamp(102.41px ,7.56vw ,130.0px)", // 130 ÷ 14.4 = 9.03vw
+                height: "clamp(33.88px ,2.5vw ,43.0px)", // 43 ÷ 14.4
                 fontFamily: "sans-serif, Noto Sans KR",
             }}
         >
@@ -33,12 +33,12 @@ const DropDown = ({ title, targetPlaceData, setCreateData, createData }) => {
                     backgroundColor: "#F5F5F5",
                     color: "rgb(117,117,117)",
                     border: "none",
-                    borderRadius: "min(15px,1.04vw)",
+                    borderRadius: "clamp(11.82px ,0.87vw ,15.0px)",
 
-                    height: "min(3.125vw, 45px)", // 45 ÷ 14.4
-                    width: "min(1.39vw, 20px)", // 20 ÷ 14.4
+                    height: "clamp(35.45px ,2.62vw ,45.0px)", // 45 ÷ 14.4
+                    width: "clamp(15.76px ,1.16vw ,20.0px)", // 20 ÷ 14.4
 
-                    fontSize: "min(1.22vw, 1.1rem)",
+                    fontSize: "clamp(13.87px ,1.02vw ,17.6px)",
                 }}
             >
                 {createData.place || title}
@@ -64,17 +64,20 @@ const DropDown = ({ title, targetPlaceData, setCreateData, createData }) => {
                         style={{
                             backgroundColor: "#ffffff",
                             color: "black",
+
+                            // 여기 못잡겠음..
                             transform: isWideScreen
                                 ? "translate(-124.9px, -7px)"
                                 : "translate(-8.67vw, -0.486vw)",
+                            // 여기 못잡겠음..
 
-                            width: "min(8.33vw, 120px)", // 120 ÷ 14.4
-                            height: "min(2.78vw, 40px)",
+                            width: "clamp(94.53px ,6.98vw ,120.0px)", // 120 ÷ 14.4
+                            height: "clamp(31.51px ,2.33vw ,40.0px)",
 
                             border: "1px solid rgb(210,210,210)",
 
-                            paddingTop: "min(0.49vw, 7px)",
-                            fontSize: "min(1.11vw,16px)",
+                            paddingTop: "clamp(5.51px ,0.41vw ,7.0px)",
+                            fontSize: "clamp(12.6px ,0.93vw ,16.0px)",
                         }}
                     >
                         {item.place}
